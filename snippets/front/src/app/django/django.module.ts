31b5aa83fc5dc23c5df0b85d4d15b4c4d633931c
Nekmo <contacto@nekmo.com>
2018-05-07 03:24:33 +0200

    Django module.

---
+++ snippets/front/src/app/django/django.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ApiService} from "./api.service";
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+      ApiService,
+  ]
+})
+export class DjangoModule { }
