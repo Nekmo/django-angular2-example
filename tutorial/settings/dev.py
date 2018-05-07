b076a45ef859f24682387d9bf5e37021c978801a
Nekmo <contacto@nekmo.com>
2018-05-07 03:01:54 +0200

    Actualizado a Angular 6.0 y Django CORS.

---
+++ tutorial/settings/dev.py
@@ -0,0 +1,8 @@
+from .defaults import *
+
+INSTALLED_APPS.append('corsheaders')
+MIDDLEWARE.append('django.middleware.common.CommonMiddleware')
+
+# CELERY_TASK_ALWAYS_EAGER = True
+CORS_ORIGIN_ALLOW_ALL = True
+ALLOWED_HOSTS = ['*']
