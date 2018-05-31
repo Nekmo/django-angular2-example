88b132183fcdbbaf0d9aad9dc43680187374d241
Nekmo <contacto@nekmo.com>
2018-06-01 01:43:05 +0200

    Cambiada ubicación caja búsqueda.

--- tutorial/settings/dev.py
+++ tutorial/settings/dev.py
@@ -7,3 +7,10 @@ DEBUG = True
 # # CELERY_TASK_ALWAYS_EAGER = True
 # CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['*']
+
+try:
+    import django_angular2
+except ImportError:
+    pass
+else:
+    INSTALLED_APPS.append('django_angular2')
